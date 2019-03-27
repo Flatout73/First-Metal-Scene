5fe7e9b85f6e6f9b17c4fef77aabfa514ee250fe
Flatout73 <fantom2259@gmail.com>
2019-03-27 04:29:03 +0300

    Skybox

---
+++ MetalTest/Assets/SkyBox/SkyboxHeader.h
@@ -0,0 +1,13 @@
+//
+//  SkyboxHeader.h
+//  MetalTest
+//
+//  Created by Леонид Лядвейкин on 27/03/2019.
+//  Copyright © 2019 Леонид Лядвейкин. All rights reserved.
+//
+
+#ifndef SkyboxHeader_h
+#define SkyboxHeader_h
+
+
+#endif /* SkyboxHeader_h */
---
+++ MetalTest/CarNode.swift
@@ -0,0 +1,9 @@
+//
+//  CarNode.swift
+//  MetalTest
+//
+//  Created by Леонид Лядвейкин on 24/03/2019.
+//  Copyright © 2019 Леонид Лядвейкин. All rights reserved.
+//
+
+import Foundation
